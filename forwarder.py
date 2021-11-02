8b9cbe4cf14a23808e66322680c9b709a58f7c79
Aaron <fireninja108@gmail.com>
2021-11-01 21:43:05 -0400

    stdout reader

--- forwarder.py
+++ forwarder.py
@@ -7,21 +7,28 @@ import re
 import struct
 import aprslib
 import socket
-
+import time
+import sys
 
 IP = "127.0.0.1"
 PORT = 8081
 
+def output_reader():
+    while True:
+        line = input()
 
+        if re.match("^(?P<call>.+)>(?P<dest>.+),", line):
+            line = line.split(" ")
+            packet = line[1]
+            break
+
+        else:
+            print("Invalid")
+            continue
+
+    return packet
+    
 def sender(parsed):
-    # raw = parsed["raw"]
-    # callsign = parsed["from"]
-    # receiver = parsed["to"]
-    # latitude = parsed["latitude"]
-    # longitude = parsed["longitude"]
-    # message = parsed["comment"]
-    # sender_to_receiver = "./aprs_fwd %s:%s %s " %(IP, PORT, callsign)
-
     latitude = parsed["latitude"]
     longitude = parsed["longitude"]
 
@@ -32,14 +39,17 @@ def sender(parsed):
     sock.sendto(lat_long, (IP, PORT))
 
 
-
 def main():
     packet = r"KD2WSM-5>APDR16,:=4307.61N07741.17WS433.290MHz" 
     parsed = aprslib.parse("KD2WSM-5>APDR16:!4307.61N\\07741.17WS433.290MHz")
 
     while True:
-        sender(parsed)
-        # time.sleep(5)
+        APRS_packet = output_reader()
+        print(APRS_packet)
+        parsed_packet = aprslib.parse(APRS_packet)
+        sender(parsed_packet)
+        print("Sent")
+        time.sleep(5)
     
     
 
